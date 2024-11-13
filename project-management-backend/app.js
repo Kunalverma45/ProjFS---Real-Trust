f9f77918da40a7a022d26b9cb73ecd0c1c2ad5c7
Kunal Verma <kv47871@gmail.com>
2024-11-13 19:39:39 +0530

    Adding Extra docs and files for better Frontend and Backend - Updated Version

---
+++ project-management-backend/app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const projectRoutes = require("./routes/projects");
+const clientRoutes = require("./routes/clients");
+const contactRoutes = require("./routes/contact");
+const subscriptionRoutes = require("./routes/subscriptions");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+  });
+  
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors(
+    {
+        origin: ["https://real-trust-alpha.vercel.app"],
+        methods:["POST","GET"],
+        credentials:true
+    }
+));
+app.use("/uploads", express.static("uploads")); // Serve static files
+
+// Routes
+app.use("/api/projects", projectRoutes);
+app.use("/api/clients", clientRoutes);
+app.use("/api/contact", contactRoutes);
+app.use("/api/subscriptions", subscriptionRoutes);
+
+// Database Connection   
+mongoose
+  .connect("mongodb+srv://riyaraghuwanshi0831:riyaFullstackPass@fullstack-mern.grep1.mongodb.net/real-trust?retryWrites=true&w=majority&appName=Fullstack-Mern", {
+
+    })
+  .then(() => console.log("MongoDB connected - http://localhost:5000/"))
+  .catch((err) => console.log(err));
+
+module.exports = app;
